ad41031a15ed9087554b505bebd9ce8f3e2d1bf2
mahmutenesaydin <mahmutenes19032010@hotmail.com>
2021-01-28 16:11:00 +0300

    Factory Method
    Factory Method Design Pattern Example

---
+++ ŞA - FactoryMethodDP/ŞA - FactoryMethodDP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ŞA___FactoryMethodDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dokuman[] dokumanlar = new Dokuman[]
+            {
+                new Rapor(),
+                new Ozgecmis()
+            };
+            foreach (Dokuman d in dokumanlar)
+            {
+                Console.WriteLine("{0} --", d.ToString());
+                foreach (Sayfa s in d.Sayfalar)
+                {
+                    Console.WriteLine(" " + s.ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
